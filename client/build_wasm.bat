54edc769195390b01e45478634af8740d24698d3
Fil Krynicki <filipkrynicki@gmail.com>
2022-02-23 21:15:29 -0500

    workflow improvements
    + 'makefile' invokes cargo
    + calling module function from javascript

--- client/build_wasm.bat
+++ client/build_wasm.bat
@@ -1,6 +1,7 @@
 set wasmfile=rs\target\wasm32-unknown-unknown\debug\gamechooser2_client.wasm
 set outdir=rs-wasm-bindgen-output
 
-mkdir outdir
+cargo build --manifest-path=rs/Cargo.toml --target wasm32-unknown-unknown
+mkdir %outdir%
 wasm-bindgen\wasm-bindgen.exe --target web --no-typescript --out-dir %outdir% %wasmfile%
 copy %wasmfile% %outdir%
