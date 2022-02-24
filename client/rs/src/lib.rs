ae4291cef72893f0384b5509cbadf525d045ad99
Fil Krynicki <filipkrynicki@gmail.com>
2022-02-23 21:54:07 -0500

    modifying DOM from Rust!

--- client/rs/src/lib.rs
+++ client/rs/src/lib.rs
@@ -2,10 +2,19 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
-    fn alert(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, modified string!");
+pub fn cycle_tag_tri_box(element: &web_sys::HtmlSpanElement) {
+    match element.text_content() {
+        Some(tc) => {
+            match tc.as_str() {
+                "☐" => element.set_text_content(Some("☑")),
+                "☑" => element.set_text_content(Some("☒")),
+                "☒" => element.set_text_content(Some("☐")),
+                _ => element.set_text_content(Some("☐")),
+            }
+        },
+        None => element.set_text_content(Some("☐")),
+    }
 }
