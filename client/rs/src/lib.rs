7b91efc9860ba9f2208a835924c353cbbf86ef8c
Fil Krynicki <filipkrynicki@gmail.com>
2022-04-07 23:31:27 -0400

    WIP refactoring search_igdb to help create some more ergonomic web_sys bindings

--- client/rs/src/lib.rs
+++ client/rs/src/lib.rs
@@ -3,6 +3,7 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{Request, RequestInit, RequestMode, Response};
+use web_sys::{HtmlDivElement, HtmlImageElement, HtmlInputElement, HtmlParagraphElement, HtmlSpanElement};
 
 use gamechooser_core as core;
 
@@ -10,6 +11,15 @@ macro_rules! weblog {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
     }
+}
+
+trait TDOMElement {
+    fn tag() -> &'static str;
+}
+
+trait TErgonomicDocument {
+    fn create_element_typed<T: JsCast + TDOMElement>(&self) -> Result<T, JsError>;
+    fn get_typed_element_by_id<T: JsCast>(&self, id: &str) -> Result<T, JsError>;
 }
 
 #[allow(dead_code)]
@@ -78,8 +88,41 @@ impl<T> TUnpackJSError for Result<T, JsValue> {
     }
 }
 
+impl TErgonomicDocument for web_sys::Document {
+    fn create_element_typed<T: JsCast + TDOMElement>(&self) -> Result<T, JsError> {
+        let elem = document().create_element(T::tag()).or(Err(JsError::new("Failed to create element")))?;
+
+        match elem.dyn_into::<T>() {
+            Ok(res) => Ok(res),
+            Err(_) => Err(JsError::new("Created an element, but it somehow had the wrong type."))
+        }
+    }
+
+    fn get_typed_element_by_id<T: JsCast>(&self, id: &str) -> Result<T, JsError> {
+        let elem = document().get_element_by_id(id).ok_or(JsError::new("Element did not exist"))?;
+
+        match elem.dyn_into::<T>() {
+            Ok(res) => Ok(res),
+            Err(_) => Err(JsError::new("get_typed_element_by_id found element, but it wasn't the desired type"))
+        }
+    }
+}
+
+// -- I think this is necessary for something
 #[wasm_bindgen]
 extern {
+}
+
+fn window() -> web_sys::Window {
+    let window = web_sys::window().expect("no global `window` exists");
+    window
+}
+
+fn document() -> web_sys::Document {
+    let window = window();
+    let document = window.document().expect("should have a document on window");
+
+    document
 }
 
 #[wasm_bindgen]
@@ -145,53 +188,46 @@ pub async fn twitch_api_test() {
 }
 
 #[wasm_bindgen]
-pub async fn search_igdb() -> Result<(), String> {
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
+pub async fn search_igdb() -> Result<(), JsError> {
+    let window = window();
+    let document = document();
 
     // -- do the request
     let games : Vec<core::SGame> = {
     //let text_string = {
-        let name_search_string = &document.get_element_by_id("name_search_string").unwrap().dyn_into::<web_sys::HtmlInputElement>().unwrap();
+        let name_search_input = &document.get_typed_element_by_id::<HtmlInputElement>("name_search_string")?;
 
         let mut opts = RequestInit::new();
         opts.method("POST");
         opts.mode(RequestMode::Cors);
 
-        let url = format!("http://localhost:8000/search_igdb/{}", name_search_string.value().as_str());
-        let request = Request::new_with_str_and_init(&url, &opts).js_error()?;
+        let url = format!("http://localhost:8000/search_igdb/{}", name_search_input.value().as_str());
+        let request = Request::new_with_str_and_init(&url, &opts).or(Err(JsError::new("Failed to create request")))?;
 
-        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await.stomp_err(String::from("Fetch failed"))?;
+        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
         assert!(resp_value.is_instance_of::<Response>());
-        let resp: Response = resp_value.dyn_into().stomp_err(String::from("resp was not a Response"))?;
+        let resp: Response = resp_value.dyn_into()?;
 
-        /*
-        let text_promise = resp.text().stomp_err(String::from("Couldn't get text from response - was it bad?"))?;
-        let text = JsFuture::from(text_promise).await.stomp_err(String::from("Couldn't get text from response"))?;
-
-        text.as_string().ok_or(String::from("text was not a string"))?
-        */
-
-        let json_promise = resp.json().js_error()?;
-        let json = JsFuture::from(json_promise).await.js_error()?;
+        let json_promise = resp.json()?;
+        let json = JsFuture::from(json_promise).await?;
 
         json.into_serde().unwrap()
     };
 
-    let output_elem = &document.get_element_by_id("search_igdb_output").unwrap().dyn_into::<web_sys::HtmlDivElement>().unwrap();
+    let output_elem = document.get_typed_element_by_id::<HtmlDivElement>("search_igdb_output")?;
     output_elem.set_inner_html("");
     for game in &games {
-        let game_div = document.create_element("div").js_error()?.dyn_into::<web_sys::HtmlDivElement>().unwrap();
-        output_elem.append_child(&game_div).js_error()?;
+        let game_div = document.create_element_typed::<HtmlDivElement>()?;
+        output_elem.append_child(&game_div)?;
 
-        let title_elem = document.create_element("h3").js_error()?;
+        let title_elem = document.create_element("h3")?;
         title_elem.set_text_content(Some(game.title()));
-        game_div.append_child(&title_elem).js_error()?;
+        game_div.append_child(&title_elem)?;
 
         if let Some(url) = game.cover_url() {
-            let img_elem = document.create_element("img").js_error()?.dyn_into::<web_sys::HtmlImageElement>().unwrap();
+            let img_elem = document.create_element_typed::<HtmlImageElement>()?;
             img_elem.set_src(url);
-            game_div.append_child(&img_elem).js_error()?;
+            game_div.append_child(&img_elem)?;
         }
     }
 
