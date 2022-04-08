0c5e104b66a1dc510f8fabfef3438242db6fa1d3
Fil Krynicki <filipkrynicki@gmail.com>
2022-04-07 22:22:25 -0400

    search_igdb endpoint exists and works! Time to show results in web client!

--- gamechooser-core/src/lib.rs
+++ gamechooser-core/src/lib.rs
@@ -1,3 +1,64 @@
+use chrono;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SGameTags {
+    couch_playable: Option<bool>,
+    portable_playable: Option<bool>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SOwn {
+    steam: bool,
+    egs: bool,
+    emulator: bool,
+
+    ds: bool,
+    n3ds: bool,
+    wii: bool,
+    wiiu: bool,
+    switch: bool,
+
+    ps4: bool,
+    ps5: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SGame {
+    internal_id: Option<u64>,
+    title: String,
+    release_date: Option<chrono::naive::NaiveDate>,
+    igdb_id: Option<u64>,
+    cover_url: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SCollectionGame {
+    game: SGame,
+
+    via: String,
+
+    tags: SGameTags,
+    own: SOwn,
+
+    next_valid_proposal_date: chrono::naive::NaiveDate,
+    retired: bool,
+    passes: u16,
+    ignore_passes: bool,
+}
+
+impl SGame {
+    pub fn new_igdb(title: String, release_date: Option<chrono::naive::NaiveDate>, igdb_id: u64, cover_url: Option<String>) -> Self {
+        Self {
+            internal_id: None,
+            title,
+            release_date,
+            igdb_id: Some(igdb_id),
+            cover_url,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,15 @@
 #[macro_use] extern crate rocket;
 
 //use reqwest;
-use async_std::fs::File;
-use futures::stream::StreamExt;
+use tokio::fs::File;
+use tokio_stream::StreamExt;
 use serde::{Serialize, Deserialize};
 use serde::de::{DeserializeOwned};
 use std::error::{Error};
 use std::result::{Result};
+use rocket::serde::json::Json as RocketJson;
+
+use gamechooser_core as core;
 
 #[derive(Debug, Serialize, Clone)]
 pub struct STwitchOauthTokenRequest {
@@ -62,6 +65,7 @@ pub trait TConfigStore {
 #[derive(Clone)]
 struct SReqwestTwitchAPISession {
     client: reqwest::Client,
+    twitch_client_id: String,
     token_info: Option<STwitchOauthTokenResponse>,
 }
 
@@ -76,6 +80,18 @@ struct SConfigFile {
 }
 
 impl SReqwestTwitchAPIClient {
+    async fn new_session() -> Result<SReqwestTwitchAPISession, String> {
+        let cfg : SConfigFile = confy::load("gamechooser2_cli_client").unwrap();
+
+        let params = STwitchOauthTokenRequest{
+            client_id: cfg.twitch_client_id,
+            client_secret: cfg.twitch_client_secret,
+            grant_type: "client_credentials",
+        };
+
+        Self::init(params).await
+    }
+
     fn prepare_request(session: &SReqwestTwitchAPISession, rb: STwitchAPIRequestBuilder) -> reqwest::RequestBuilder {
         let mut request = session.client.post(rb.url);
 
@@ -104,6 +120,7 @@ impl SReqwestTwitchAPIClient {
                 println!("{:?}", resp);
                 Ok(SReqwestTwitchAPISession{
                     client,
+                    twitch_client_id: params.client_id,
                     token_info: Some(resp),
                 })
             },
@@ -111,10 +128,10 @@ impl SReqwestTwitchAPIClient {
         }
     }
 
-    async fn post_interp_json<T: DeserializeOwned>(session: SReqwestTwitchAPISession, rb: STwitchAPIRequestBuilder) -> Result<T, String> {
+    async fn post_interp_json<T: DeserializeOwned>(session: SReqwestTwitchAPISession, rb: STwitchAPIRequestBuilder) -> Result<T, Box<dyn Error>> {
         let req = Self::prepare_request(&session, rb);
-        let resp = req.send().await.unwrap();
-        Ok(resp.json().await.unwrap())
+        let resp = req.send().await?;
+        Ok(resp.json().await?)
     }
 
     async fn post_text(session: SReqwestTwitchAPISession, rb: STwitchAPIRequestBuilder) -> Result<String, String> {
@@ -129,19 +146,11 @@ impl SReqwestTwitchAPIClient {
 }
 
 async fn test_twitch_api() -> Result<String, String> {
-    let cfg : SConfigFile = confy::load("gamechooser2_cli_client").unwrap();
-
-    let params = STwitchOauthTokenRequest{
-        client_id: cfg.twitch_client_id,
-        client_secret: cfg.twitch_client_secret,
-        grant_type: "client_credentials",
-    };
-
-    let session = SReqwestTwitchAPIClient::init(params.clone()).await?;
+    let session = SReqwestTwitchAPIClient::new_session().await?;
 
     let request = STwitchAPIRequestBuilder::new()
         .url("https://api.igdb.com/v4/search/")
-        .header("Client-ID", params.client_id.as_str())
+        .header("Client-ID", session.twitch_client_id.as_str())
         .header("Authorization", format!("Bearer {}", SReqwestTwitchAPIClient::access_token(&session)).as_str())
         .body("search \"Halo\"; fields game,name;");
 
@@ -197,10 +206,68 @@ async fn index() -> Result<String, String> {
 #[post("/test")]
 async fn test() -> Result<String, String> {
     //test_twitch_api().await
-    match test_csv().await {
+    match test_twitch_api().await {
         Ok(s) => Ok(s),
         Err(e) => Err(e.to_string())
     }
+}
+
+#[post("/search_igdb/<name>")]
+async fn search_igdb(name: &str) -> Result<RocketJson<Vec<core::SGame>>, String> {
+    let session = SReqwestTwitchAPIClient::new_session().await?;
+
+    #[derive(Deserialize)]
+    struct SIGDBSearchResultCover {
+        id: u64,
+        url: String,
+    }
+    #[derive(Deserialize)]
+    struct SIGDBSearchResult {
+        id: u64,
+        name: String,
+        first_release_date: Option<i64>,
+        cover: Option<SIGDBSearchResultCover>,
+    }
+
+    let search_results : Vec<SIGDBSearchResult> = {
+        let body = format!("search \"{}\"; fields name,first_release_date,cover.url;", name);
+
+        /*
+        Should be equivalent to:
+        curl -d "search \"halo\"; fields name,first_release_date,cover.url;" -H 'Client-ID: <ID>' -H 'Authorization: Bearer <BEARER>' -H 'Accept: application/json' https://api.igdb.com/v4/games
+        */
+
+        let request = STwitchAPIRequestBuilder::new()
+            .url("https://api.igdb.com/v4/games/")
+            .header("Client-ID", session.twitch_client_id.as_str())
+            .header("Authorization", format!("Bearer {}", SReqwestTwitchAPIClient::access_token(&session)).as_str())
+            .header("Accept", "application/json")
+            .body(body.as_str());
+
+        match SReqwestTwitchAPIClient::post_interp_json::<Vec<SIGDBSearchResult>>(session.clone(), request).await {
+            Ok(res) => Ok(res),
+            Err(e) => Err(format!("Failed with error {:?}", e)),
+        }
+    }?;
+
+    fn timestamp_to_chrono(ts: i64) -> chrono::naive::NaiveDate {
+        chrono::naive::NaiveDateTime::from_timestamp(ts, 0).date()
+    }
+    fn extract_cover_url(cover: SIGDBSearchResultCover) -> String {
+        cover.url
+    }
+
+    let mut results = Vec::with_capacity(search_results.len());
+    for search_res in search_results {
+        results.push(core::SGame::new_igdb(
+            search_res.name,
+            search_res.first_release_date.map(timestamp_to_chrono),
+            search_res.id,
+            search_res.cover.map(extract_cover_url),
+        ));
+    }
+
+    Ok(RocketJson(results))
 }
 
 #[launch]
@@ -209,5 +276,5 @@ fn rocket() -> _ {
 
     rocket::build()
         .mount("/static", rocket::fs::FileServer::from("../client/served_files"))
-        .mount("/", routes![index, test])
+        .mount("/", routes![index, test, search_igdb])
 }
