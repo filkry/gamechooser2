6f108e4039e6edd33ac298204868f50f6bfcbbc2
Fil Krynicki <filipkrynicki@gmail.com>
2022-04-09 10:48:09 -0400

    Some simple ergonomic additions for web-sys

--- client/rs/src/lib.rs
+++ client/rs/src/lib.rs
@@ -1,3 +1,5 @@
+mod web;
+
 //use console_error_panic_hook;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::{JsCast, JsValue};
@@ -6,20 +8,12 @@ use web_sys::{Request, RequestInit, RequestMode, Response};
 use web_sys::{HtmlDivElement, HtmlImageElement, HtmlInputElement, HtmlParagraphElement, HtmlSpanElement};
 
 use gamechooser_core as core;
+use web::{document, window, TToJsError, TErgonomicDocument};
 
 macro_rules! weblog {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
     }
-}
-
-trait TDOMElement {
-    fn tag() -> &'static str;
-}
-
-trait TErgonomicDocument {
-    fn create_element_typed<T: JsCast + TDOMElement>(&self) -> Result<T, JsError>;
-    fn get_typed_element_by_id<T: JsCast>(&self, id: &str) -> Result<T, JsError>;
 }
 
 #[allow(dead_code)]
@@ -53,76 +47,9 @@ impl ETagQuery {
     }
 }
 
-trait TStompErr {
-    type OkType;
-
-    fn stomp_err<N>(self, e: N) -> Result<Self::OkType, N>;
-}
-
-impl<T, E> TStompErr for Result<T, E> {
-    type OkType = T;
-
-    fn stomp_err<N>(self, e: N) -> Result<Self::OkType, N> {
-        match self {
-            Ok(t) => Ok(t),
-            Err(_) => Err(e),
-        }
-    }
-}
-
-trait TUnpackJSError {
-    type OkType;
-
-    fn js_error(self) -> Result<Self::OkType, String>;
-}
-
-fn js_error_to_string(js_error: JsValue) -> String {
-    format!("{:?}", js_error)
-}
-
-impl<T> TUnpackJSError for Result<T, JsValue> {
-    type OkType = T;
-
-    fn js_error(self) -> Result<Self::OkType, String> {
-        self.map_err(js_error_to_string)
-    }
-}
-
-impl TErgonomicDocument for web_sys::Document {
-    fn create_element_typed<T: JsCast + TDOMElement>(&self) -> Result<T, JsError> {
-        let elem = document().create_element(T::tag()).or(Err(JsError::new("Failed to create element")))?;
-
-        match elem.dyn_into::<T>() {
-            Ok(res) => Ok(res),
-            Err(_) => Err(JsError::new("Created an element, but it somehow had the wrong type."))
-        }
-    }
-
-    fn get_typed_element_by_id<T: JsCast>(&self, id: &str) -> Result<T, JsError> {
-        let elem = document().get_element_by_id(id).ok_or(JsError::new("Element did not exist"))?;
-
-        match elem.dyn_into::<T>() {
-            Ok(res) => Ok(res),
-            Err(_) => Err(JsError::new("get_typed_element_by_id found element, but it wasn't the desired type"))
-        }
-    }
-}
-
 // -- I think this is necessary for something
 #[wasm_bindgen]
 extern {
-}
-
-fn window() -> web_sys::Window {
-    let window = web_sys::window().expect("no global `window` exists");
-    window
-}
-
-fn document() -> web_sys::Document {
-    let window = window();
-    let document = window.document().expect("should have a document on window");
-
-    document
 }
 
 #[wasm_bindgen]
@@ -140,7 +67,7 @@ pub fn cycle_tag_tri_box(element: &web_sys::HtmlSpanElement) {
     }
 }
 
-async fn call_test() -> Result<String, String> {
+async fn call_test() -> Result<String, JsError> {
     let window = web_sys::window().expect("no global `window` exists");
 
     let mut opts = RequestInit::new();
@@ -149,21 +76,21 @@ async fn call_test() -> Result<String, String> {
 
     let url = format!("http://localhost:8000/test");
     //let request = Request::new_with_str_and_init(&url, &opts).stomp_err(String::from("Failed to make request"))?;
-    let request = Request::new_with_str_and_init(&url, &opts).js_error()?;
+    let request = Request::new_with_str_and_init(&url, &opts).to_jserr()?;
 
-    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await.stomp_err(String::from("Fetch failed"))?;
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await.to_jserr()?;
     assert!(resp_value.is_instance_of::<Response>());
-    let resp: Response = resp_value.dyn_into().stomp_err(String::from("resp was not a Response"))?;
-    let text_promise = resp.text().stomp_err(String::from("Couldn't get text from response - was it bad?"))?;
-    let text = JsFuture::from(text_promise).await.stomp_err(String::from("Couldn't get text from response"))?;
+    let resp: Response = resp_value.dyn_into().to_jserr()?;
+    let text_promise = resp.text().to_jserr()?;
+    let text = JsFuture::from(text_promise).await.to_jserr()?;
 
-    let text_string = text.as_string().ok_or(String::from("text was not a string"))?;
+    let text_string = text.as_string().ok_or(JsError::new("text was not a string"))?;
 
     Ok(text_string)
 }
 
 #[wasm_bindgen]
-pub async fn twitch_api_test() {
+pub async fn twitch_api_test() -> Result<(), JsError> {
     weblog!("twitch_api_test started");
 
     let window = web_sys::window().expect("no global `window` exists");
@@ -173,18 +100,13 @@ pub async fn twitch_api_test() {
 
     if let Ok(p) = elem.clone().dyn_into::<web_sys::HtmlParagraphElement>() {
         p.set_inner_text("API test started 333");
-
-        let response_string = call_test().await;
-
-        if let Err(e) = response_string {
-            weblog!("test failed with error: {:?}", e);
-            return;
-        }
-
-        p.set_inner_text(response_string.unwrap().as_str());
+        let response_string = call_test().await?;
+        p.set_inner_text(response_string.as_str());
     }
 
     weblog!("twitch_api_test end reached");
+
+    Ok(())
 }
 
 #[wasm_bindgen]
@@ -204,12 +126,12 @@ pub async fn search_igdb() -> Result<(), JsError> {
         let url = format!("http://localhost:8000/search_igdb/{}", name_search_input.value().as_str());
         let request = Request::new_with_str_and_init(&url, &opts).or(Err(JsError::new("Failed to create request")))?;
 
-        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+        let resp_value = JsFuture::from(window.fetch_with_request(&request)).await.or(Err(JsError::new("Fetch failed")))?;
         assert!(resp_value.is_instance_of::<Response>());
-        let resp: Response = resp_value.dyn_into()?;
+        let resp: Response = resp_value.dyn_into().or(Err(JsError::new("resp_value is not a Response")))?;
 
-        let json_promise = resp.json()?;
-        let json = JsFuture::from(json_promise).await?;
+        let json_promise = resp.json().or(Err(JsError::new("Could not get json promise from Response")))?;
+        let json = JsFuture::from(json_promise).await.or(Err(JsError::new("Could not convert response to json")))?;
 
         json.into_serde().unwrap()
     };
@@ -218,16 +140,16 @@ pub async fn search_igdb() -> Result<(), JsError> {
     output_elem.set_inner_html("");
     for game in &games {
         let game_div = document.create_element_typed::<HtmlDivElement>()?;
-        output_elem.append_child(&game_div)?;
+        output_elem.append_child(&game_div).to_jserr()?;
 
-        let title_elem = document.create_element("h3")?;
+        let title_elem = document.create_element("h3").to_jserr()?;
         title_elem.set_text_content(Some(game.title()));
-        game_div.append_child(&title_elem)?;
+        game_div.append_child(&title_elem).to_jserr()?;
 
         if let Some(url) = game.cover_url() {
             let img_elem = document.create_element_typed::<HtmlImageElement>()?;
             img_elem.set_src(url);
-            game_div.append_child(&img_elem)?;
+            game_div.append_child(&img_elem).to_jserr()?;
         }
     }
 
---
+++ client/rs/src/web.rs
@@ -0,0 +1,93 @@
+use wasm_bindgen::{JsCast, JsValue, JsError};
+use web_sys::{HtmlDivElement, HtmlImageElement, HtmlInputElement, HtmlParagraphElement, HtmlSpanElement};
+
+pub trait TErgonomicDocument {
+    fn create_element_typed<T: JsCast + TDOMElement>(&self) -> Result<T, JsError>;
+    fn get_typed_element_by_id<T: JsCast>(&self, id: &str) -> Result<T, JsError>;
+}
+
+trait TDOMElement {
+    fn tag() -> &'static str;
+}
+
+pub trait TToJsError {
+    type OkType;
+
+    fn to_jserr(self) -> Result<Self::OkType, JsError>;
+}
+
+// -- Implementations start here
+
+impl TDOMElement for HtmlDivElement {
+    fn tag() -> &'static str {
+        "div"
+    }
+}
+impl TDOMElement for HtmlImageElement {
+    fn tag() -> &'static str {
+        "img"
+    }
+}
+impl TDOMElement for HtmlInputElement {
+    fn tag() -> &'static str {
+        "input"
+    }
+}
+impl TDOMElement for HtmlParagraphElement {
+    fn tag() -> &'static str {
+        "p"
+    }
+}
+impl TDOMElement for HtmlSpanElement {
+    fn tag() -> &'static str {
+        "span"
+    }
+}
+
+impl<T> TToJsError for Result<T, JsValue> {
+    type OkType = T;
+
+    fn to_jserr(self) -> Result<Self::OkType, JsError> {
+        match self {
+            Ok(o) => Ok(o),
+            Err(e) => {
+                let temp = format!("{:?}", e);
+                Err(JsError::new(temp.as_str()))
+            }
+        }
+    }
+}
+
+impl TErgonomicDocument for web_sys::Document {
+    fn create_element_typed<T: JsCast + TDOMElement>(&self) -> Result<T, JsError> {
+        let elem = document().create_element(T::tag()).or(Err(JsError::new("Failed to create element")))?;
+
+        match elem.dyn_into::<T>() {
+            Ok(res) => Ok(res),
+            Err(_) => Err(JsError::new("Created an element, but it somehow had the wrong type."))
+        }
+    }
+
+    fn get_typed_element_by_id<T: JsCast>(&self, id: &str) -> Result<T, JsError> {
+        let elem = document().get_element_by_id(id).ok_or(JsError::new("Element did not exist"))?;
+
+        match elem.dyn_into::<T>() {
+            Ok(res) => Ok(res),
+            Err(_) => Err(JsError::new("get_typed_element_by_id found element, but it wasn't the desired type"))
+        }
+    }
+}
+
+pub fn window() -> web_sys::Window {
+    let window = web_sys::window().expect("no global `window` exists");
+    window
+}
+
+pub fn document() -> web_sys::Document {
+    let window = window();
+    let document = window.document().expect("should have a document on window");
+
+    document
+}
+
+
